361196eff7608cab73a581bf67392b3d9fcac740
gabrielgomes-coder <ggfernandes30@gmail.com>
2023-09-19 18:46:09 -0300

    Versão final até o momento

--- spotify/src/views/FaqView.vue
+++ spotify/src/views/FaqView.vue
@@ -1,8 +1,5 @@
 <script setup>
-import Footer from '../components/Footer.vue';
-import NavBar from '../components/NavBar.vue';
 import PerguntaCom from '../components/PerguntaCom.vue';
-
 </script>
 
 <template>
